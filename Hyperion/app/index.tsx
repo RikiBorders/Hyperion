7e3a2aa1f0146088f65103d9da92e6f69359f71f
RikiBorders <ninjab02@hotmail.com>
2025-11-10 05:14:44 +0000

    Fix asset paths

--- Hyperion/app/index.tsx
+++ Hyperion/app/index.tsx
@@ -1,9 +1,8 @@
 
 import React, { useState } from 'react'
-import { Text, View, Button } from 'react-native'
-import styles from './index.styles'
-import GenericModal from './Components/Modal/Modal'
+import { Button, Text, View } from 'react-native'
 import Navbar from './Components/Navbar/Navbar'
+import styles from './index.styles'
 import Projects from './Pages/Projects/projects'
 
 export default function Index() {
@@ -17,19 +16,6 @@ export default function Index() {
         <View style={styles.container}>
           <Text style={styles.title}>Modal demo</Text>
           <Button title="Open Modal" onPress={() => setOpen(true)} />
-
-          <GenericModal
-            visible={open}
-            onRequestClose={() => setOpen(false)}
-            horizontal={'85%'}
-            vertical={300}
-            animationType="slide"
-          >
-            <View style={styles.modalContent}>
-              <Text style={styles.modalText}>This is a demo modal.</Text>
-              <Button title="Close" onPress={() => setOpen(false)} />
-            </View>
-          </GenericModal>
         </View>
       ) : (
         <Projects />
